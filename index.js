1f4cac7ceee23b464b7b9395db3c715a7c3677f2
ValentineSokol <valentinesokolovskiy@gmail.com>
2022-05-19 16:40:43 +0300

    Added the ability to run terminal commands.

--- index.js
+++ index.js
@@ -3,10 +3,51 @@ const { terminal } = require('terminal-kit');
 const os = require('os');
 const fs = require('fs/promises');
 const path = require('path');
+const childProcess = require('child_process');
 const open = require('open');
-
 async function readDir(basePath) {
-    terminal.clear();
+    let isTerminalMode = false;
+    terminal.reset();
+    terminal.removeAllListeners('key');
+    terminal.on('key', (key) => {
+        function renderCommandInput() {
+          terminal.inputField((error, command) => {
+                if (!command) return renderCommandInput();
+                if (error) {
+                    terminal.clear();
+                    console.error(error);
+                    return;
+                }
+                childProcess.exec(command, { cwd: basePath }, (error, stdout) => {
+                    if (error) terminal.red(`\n ${error}`);
+                    else terminal.green(`\n ${stdout}`);
+                    renderCommandInput();
+                });
+
+            });
+        }
+        const { username } = os.userInfo();
+        if (key.toLowerCase() === 't') {
+            if (isTerminalMode) return;
+            terminal.clear();
+            terminal.green(`${username}'s terminal:`)
+            renderCommandInput();
+            isTerminalMode = !isTerminalMode;
+
+            }
+        if (key === 'CTRL_T') {
+            terminal.clear();
+            return readDir(basePath);
+        }
+        if (key === 'CTRL_C') {
+            terminal.clear();
+            terminal.green(`${username}, Thanks for using the FileExp Explorer!`);
+            return setTimeout(() => {
+                terminal.clear();
+                terminal.processExit();
+            }, 1000);
+        }
+    });
     const files = await fs.readdir(basePath, { withFileTypes: true });
     const statsPromise = files.map(direct => fs.stat(path.join(basePath, direct.name)));
     const stats = await Promise.all(statsPromise);
@@ -18,23 +59,20 @@ async function readDir(basePath) {
 
     terminal.green(`${basePath} \n`);
     terminal.gridMenu(entries, { exitOnUnexpectedKey: true }, (err, response) => {
+        const excludedKeys = [
+            't',
+            'esc',
+            'ctrl_c',
+        ];
         if (response.unexpectedKey === 'BACKSPACE') {
             const pathSegments = basePath.split(path.sep);
             pathSegments.pop();
             const newPath = pathSegments.join(path.sep);
             return readDir(newPath);
         }
-        if (response.unexpectedKey === 'CTRL_C') {
-            const { username } = os.userInfo();
-            terminal.clear();
-            terminal.green(`${username}, Thanks for using the FileExp Explorer!`);
-           return setTimeout(() => {
-                terminal.clear();
-                terminal.processExit();
-            }, 1000);
-
-        }
         if (response.unexpectedKey) {
+            const lowerKey = response.unexpectedKey.toLowerCase();
+            if (excludedKeys.includes(lowerKey)) return;
             return readDir(basePath);
         }
         const dirent = files[response.selectedIndex];
